0d80b3930fca237b37013dad7ac83e4d1ac224e7
Rahul Gusai <rgusai97@gmail.com>
2024-03-23 01:43:14 +0530

    Updated api url

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const env_vars = {
   MODE: 'prod',
   API_URL_DEV: 'http://127.0.0.1:8000',
-  API_URL_PROD: 'http://3.7.65.15',
+  API_URL_PROD: 'https://3.7.65.15',
 };
 
 export default env_vars;
