cea0318fc19ef49bfcb88a3c70698778c850b1d2
Rahul Gusai <rgusai97@gmail.com>
2024-06-14 14:41:47 +0530

    updated env vars

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const env_vars = {
   MODE: 'prod',
   API_URL_DEV: 'http://127.0.0.1:8000/api',
-  API_URL_PROD: 'https://climatecred.in/api',
+  API_URL_PROD: 'https://3.111.66.129/api',
 };
 
 export default env_vars;
